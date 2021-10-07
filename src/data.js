c7d70d9b63d97fe32429a5665d95fa8948f1c0eb
Your Name <pau.emd@gmail.com>
2021-10-07 17:29:06 -0300

    filtro sin eventos asociados

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+// export const filterChamps = (champions, clase) => {
+//   return champions.filter(champions.tags.includes(clase));
+// };
+export const filterChamps = (champions, champClass) => {
+
+  //return champions.length;
+  let filterArray = []
+  for (let i = 0; i<champions.length; i++){
+    if(champions[i].tags.includes(champClass)){
+      filterArray.push(champions[i]);
+    }
+  }
+  return filterArray;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
